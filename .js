2b2093339a6696a5d4866ba94309697def22a569
SHowardGal <u0250173@utah.edu>
2021-11-23 16:21:04 -0700

    commit

---
+++ .js
@@ -0,0 +1,11 @@
+var shopper = {
+    name: "John", 
+    wallet: 100, 
+    hasMoney: true,
+    buy: function() {
+        return this.name + "is buying something";
+
+    },
+    groceryCart: ["milk", "eggs", "juice"]
+}
+console.log()
